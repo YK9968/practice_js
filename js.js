02f7df6135494b9c4a6274e98817cef56514af7a
YK9968 <robka4an@gmail.com>
2024-01-12 17:20:55 +0200

    test

--- js.js
+++ js.js
@@ -1,11 +1,51 @@
-function friend(friends) {
-    const notNumber = friends.filter((number) => isNaN(number))
-    const myFriend = notNumber.filter((name) => name.length <= 4)
-    console.log(myFriend);
-}
+const allCars = [
+  { make: 'Honda', model: 'CR-V', amount: 14, price: 24045 },
+  { make: 'Honda', model: 'Accord', amount: 2, price: 22455 },
+  { make: 'Mazda', model: 'Mazda 6', amount: 8, price: 24195 },
+  { make: 'Mazda', model: 'CX-9', amount: 7, price: 31520 },
+  { make: 'Toyota', model: '4Runner', amount: 19, price: 34210 },
+  { make: 'Toyota', model: 'Sequoia', amount: 16, price: 45560 },
+  { make: 'Toyota', model: 'Tacoma', amount: 4, price: 24320 },
+  { make: 'Ford', model: 'F-150', amount: 11, price: 27110 },
+  { make: 'Ford', model: 'Fusion', amount: 13, price: 22120 },
+  { make: 'Ford', model: 'Explorer', amount: 6, price: 31660 },
+];
 
-friend(['Ryan', 'Kieran', 'Mark']);
-friend(['Ryan', 'Jimmy', '123', '4', 'Cool Man']);
-friend(['Jimm', 'Cari', 'aret', 'truehdnviegkwgvke', 'sixtyiscooooool']);
-friend(['Love', 'Your', 'Face', '1']);
-v
+const newArrayForEach = [];
+let obj = {};
+
+allCars.forEach(elem => {
+  obj = { model: elem.model, amount: elem.amount };
+  newArrayForEach.push(obj);
+});
+
+console.log(newArrayForEach);
+
+// -------------------------------------------------------
+// -------------------------------------------------------
+// -------------------------------------------------------
+// -------------------------------------------------------
+let newArrayMap;
+
+newArrayMap = allCars.map(elem => {
+  return elem.amount * 2;
+});
+
+console.log(newArrayMap);
+
+// -------------------------------------------------------
+// -------------------------------------------------------
+// -------------------------------------------------------
+// -------------------------------------------------------
+
+const getModal = models => {
+    return models.name
+    .find(elem => elem.price === 24195)
+};
+
+getModal(allCars);
+
+// -------------------------------------------------------
+// -------------------------------------------------------
+// -------------------------------------------------------
+// -------------------------------------------------------
