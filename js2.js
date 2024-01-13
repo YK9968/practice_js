fd532b4a2c43c2b280f6f8d517435b53d49a4a30
YK9968 <robka4an@gmail.com>
2024-01-13 22:33:55 +0200

    update

--- js2.js
+++ js2.js
@@ -1,1 +1,26 @@
+// 'use strict';
+
+// const library = {
+//   books: 1923,
+//   logBookCount() {
+//     console.log(this.books);
+//   },
+// };
+
+// console.log(library.logBookCount());
+
+
 'use strict';
+
+const library = {
+  books: 1923,
+  logBookCount() {
+    console.log(this.books);
+  },
+};
+
+function showBooks(callback) {
+  callback();
+}
+
+showBooks(library.logBookCount.bind(library));
