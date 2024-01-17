cd21cb9ae6211b70a9e1aa026faea8a727d4f4a9
YK9968 <robka4an@gmail.com>
2024-01-17 16:30:25 +0200

    practice 17.01

--- js-class-get-set-private.js
+++ js-class-get-set-private.js
@@ -1,9 +1,37 @@
-class User {
-  constructor(name) {
-    this.name = name
-  }
-}
+// class UserPrice {
+//   #price;
 
+//   constructor(name, rating, price) {
+//     this.name = name;
+//     this.rating = rating;
+//     this.#price = price;
+//   }
 
-const stunent = new User("Mike")
-console.log(stunent);
+//   getPrice() {
+//     return this.#price;
+//   }
+
+//   setPrice() {
+//     if (this.rating > 8) {
+//       this.#price = this.#price - (this.#price * 0.2);
+//     } else {
+//       this.#price = this.#price - (this.#price * 0.1);
+//     }
+//   }
+// }
+
+// class Person extends UserPrice {
+//   constructor(name, rating, price, nameBook) {
+//     super(name, rating, price);
+//     this.nameBook = nameBook;
+//   }
+
+//   getMessage() {
+//     this.setPrice();
+//     return `Hi ${this.name}, you cost ${this.nameBook}, you must pay ${this.getPrice()}}`;
+//   }
+// }
+
+// const user1 = new Person('Alex', 5, 100, 'Tales');
+
+// console.log(user1.getMessage());
