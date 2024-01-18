93aca6407b4b3b782ef9a9a86d5e533ab0560643
YK9968 <robka4an@gmail.com>
2024-01-18 14:58:32 +0200

    111

--- js-class-get-set-private.js
+++ js-class-get-set-private.js
@@ -1,37 +1,45 @@
-// class UserPrice {
-//   #price;
+// =============================================== THIS ============================================================
 
-//   constructor(name, rating, price) {
-//     this.name = name;
-//     this.rating = rating;
-//     this.#price = price;
-//   }
+// // Напишіть функцію-конструктор, яка створює об'єкт людини з властивостями ім'я і вік.
+// // Використайте this для призначення властивостей для кожного створеного об'єкта.
 
-//   getPrice() {
-//     return this.#price;
-//   }
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
 
-//   setPrice() {
-//     if (this.rating > 8) {
-//       this.#price = this.#price - (this.#price * 0.2);
-//     } else {
-//       this.#price = this.#price - (this.#price * 0.1);
-//     }
-//   }
+//   console.log(`Hello, my name ${this.name}, I am ${this.age}`);
 // }
 
-// class Person extends UserPrice {
-//   constructor(name, rating, price, nameBook) {
-//     super(name, rating, price);
-//     this.nameBook = nameBook;
-//   }
+// const person1 = new Person('Alex', 18);
+// const person2 = new Person('Bob', 28);
 
-//   getMessage() {
-//     this.setPrice();
-//     return `Hi ${this.name}, you cost ${this.nameBook}, you must pay ${this.getPrice()}}`;
-//   }
+// console.log("--------------------------------------------");
+// // Напишіть метод для об'єкта, який збільшує його вік на одиницю.
+// // Використайте this, щоб звернутися до властивості об'єкта.
+
+// const obj = {
+//   name: 'Bob',
+//   age: 21,
+//   agePlus() {
+//     return this.age + 1;
+//   },
+// };
+
+// console.log(obj.agePlus());
+
+// console.log('--------------------------------------------');
+
+// // Використовуйте call або apply, щоб викликати функцію з this,
+// // якщо потрібно передати інший об'єкт як аргумент.
+
+// const user = {
+//     name: "Mike",
+//     age: 19,
+//     login:"Mike@.com"
 // }
 
-// const user1 = new Person('Alex', 5, 100, 'Tales');
+// function getInfo() {
+//     console.log(`Hello ${this.name}, your age is ${this.age} and you have good login "${this.login}"`);
+// }
 
-// console.log(user1.getMessage());
+// getInfo.call(user)
