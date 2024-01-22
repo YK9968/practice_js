00a1416859a5adb0f6402a6b70a2d319e95f5600
YK9968 <robka4an@gmail.com>
2024-01-22 15:35:10 +0200

    аавкрногю

--- js-DOM.js
+++ js-DOM.js
@@ -1,9 +1,5 @@
-const heading = document.createElement('h1');
-headding.classList.add('title');
-heading.textContent = 'This is a heading';
-console.log(heading); // <h1 class="title">This is a heading</h1>
+const handleClick = event => {
+  console.log(event);
+};
 
-const image = document.createElement('img');
-image.src = '<https://picsum.photos/id/11/320/240>';
-image.alt = 'Nature';
-console.log(image); // <img src="<https://picsum.photos/id/11/320/240>" alt="Nature" />
+button.addEventListener('click', handleClick);
