557155cd675fc790407ce38b421b19e82140b834
YK9968 <robka4an@gmail.com>
2024-01-31 10:55:44 +0200

    ertgh

--- js-DOM2.js
+++ js-DOM2.js
@@ -42,8 +42,30 @@
 //   ButtonPassword.textContent = 'Розкрити';
 // }
 
-console.log(_.sum([4, 2, 8, 6])); // 20
-console.log(_.sum([5, 10])); // 15
+// console.log(_.sum([4, 2, 8, 6])); // 20
+// console.log(_.sum([5, 10])); // 15
 
-console.log(_.shuffle([1, 2, 3, 4])); // [4, 1, 3, 2]
-console.log(_.shuffle([1, 2, 3, 4])); // [3, 2, 1, 4]
+// console.log(_.shuffle([1, 2, 3, 4])); // [4, 1, 3, 2]
+// console.log(_.shuffle([1, 2, 3, 4])); // [3, 2, 1, 4]
+
+const user = {
+  name: 'Jacques Gluke',
+  tag: 'jgluke',
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+const {
+  name,
+  tag,
+  stats: { followers, views, likes },
+} = user;
+
+console.log(name); // Jacques Gluke
+console.log(tag); // jgluke
+console.log(followers); // 5603
+console.log(views); // 4827
+console.log(likes);
