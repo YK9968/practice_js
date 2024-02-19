e64414078cf53ed21774462c2fbaaf0c33b4e991
YK9968 <robka4an@gmail.com>
2024-02-19 18:07:40 +0200

    11

--- js/JSON.js
+++ js/JSON.js
@@ -1,13 +1,21 @@
-const date = new Date('March 16, 2030 14:25:00');
-console.log('Data:', date);
-console.log(date.getDate());
-console.log(date.getDay());
-console.log(date.getMonth());
-console.log(date.getFullYear());
-console.log(date.getHours());
-console.log(date.getMinutes());
-console.log(date.getMilliseconds());
+const options = {
+  method: 'GET',
+};
 
-
-date.setMonth(4); 
-console.log(date);
+fetch('https://jsonplaceholder.typicode.com/users', {
+  headers: {
+    Accept: 'application/json',
+  },
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  });
