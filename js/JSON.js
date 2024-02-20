1d7e992d8a932b6ca98dfd53d1a768ba8faa4ba1
YK9968 <robka4an@gmail.com>
2024-02-20 15:50:32 +0200

    fetch()

--- js/JSON.js
+++ js/JSON.js
@@ -1,21 +1,34 @@
-const options = {
-  method: 'GET',
-};
+const listEl = document.querySelector('.list');
 
-fetch('https://jsonplaceholder.typicode.com/users', {
-  headers: {
-    Accept: 'application/json',
-  },
-})
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
-  .then(data => {
-    // Data handling
-  })
-  .catch(error => {
-    // Error handling
-  });
+const KEY_API = '42471453-a4a004408e33f852748a0909e';
+const BASE_URI = 'https://pixabay.com/api/';
+const dog = 'dog+fun';
+const link = `${BASE_URI}?key=${KEY_API}&q=${dog}`;
+
+function getImg() {
+  return fetch(link)
+    .then(respon => {
+      if (!respon.ok) {
+        throw new Error(`Img error ${respon.status}`);
+      }
+      return respon.json();
+    })
+    .catch(error => {
+      alert('bad img');
+    });
+}
+getImg().then(data => {
+  const masImg = data.hits
+    .slice(0, 3)
+    .map(({ webformatURL, tags }) => {
+      return `
+      <img
+        src="${webformatURL}"
+        alt="${tags}"
+      />
+  `;
+    })
+    .join('');
+  console.log(masImg);
+  listEl.innerHTML = masImg;
+});
